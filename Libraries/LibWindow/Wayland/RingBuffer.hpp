1d709a6cac4b3c516ec8743a48f69496d4a0f289
Sreehari Sreedev <me@ssree.dev>
2024-03-17 18:07:08 -0700

    LibWindow: add RingBuffer

---
+++ Libraries/LibWindow/Wayland/RingBuffer.hpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2024, Sreehari Sreedev <me@ssree.dev>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#pragma once
+#include <memory>
+#include <cstddef>
+
+namespace LibWindow::Wayland {
+  template <typename T>
+  class RingBuffer {
+    private:
+      std::unique_ptr<T[]> m_buffer;
+
+      size_t m_head = 0;
+      size_t m_tail = 0;
+      size_t m_max_size;
+    public:
+      RingBuffer(size_t max_size);
+      void enqueue(T item);
+      T dequeue();
+      T front();
+      bool is_empty();
+      bool is_full();
+      size_t size();
+  };
+
+  template <typename T>
+  RingBuffer<T>::RingBuffer(size_t max_size) {
+    this->m_buffer = std::make_unique<T[]>(max_size);
+    this->m_max_size = max_size;
+  }
+
+  template <typename T>
+  void RingBuffer<T>::enqueue(T item) {
+    if (this->is_full()) {
+      throw std::runtime_error("buffer is full");
+    }
+
+    this->m_buffer[this->m_tail] = item;
+    this->m_tail = (this->m_tail + 1) % this->m_max_size;
+  }
+
+  template <typename T>
+  T RingBuffer<T>::dequeue() {
+    if (this->is_empty()) {
+      throw std::runtime_error("buffer is empty");
+    }
+
+    T item = this->m_buffer[this->m_head];
+
+    T nothing;
+    this->m_buffer[this->m_head] = nothing;
+
+    this->m_head = (this->m_head + 1) % this->m_max_size;
+
+    return item;
+  }
+
+  template <typename T>
+  T RingBuffer<T>::front() {
+    return this->m_buffer[this->m_head];
+  }
+
+  template <typename T>
+  bool RingBuffer<T>::is_empty() {
+    return this->m_head == this->m_tail;
+  }
+
+  template <typename T>
+  bool RingBuffer<T>::is_full() {
+    return (this->m_tail + 1) % this->m_max_size == this->m_head;
+  }
+
+  template <typename T>
+  size_t RingBuffer<T>::size() {
+    if (this->m_tail >= this->m_head) {
+      return this->m_tail - this->m_head;
+    }
+    return this->m_max_size - (this->m_head - this->m_tail);
+  }
+};
--- src/tester.cpp
+++ src/tester.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <LibWindow/Wayland/Connection.h>
 #include <LibWindow/Wayland/Message.h>
+#include <LibWindow/Wayland/RingBuffer.hpp>
 int main(int argc, char *argv[]) {
   LibWindow::Wayland::Connection c;
   LibWindow::Wayland::Message wl_display_get_registry(1, 1);
@@ -9,5 +10,42 @@ int main(int argc, char *argv[]) {
   wl_display_get_registry.push(wl_registry_id);
   c.send_message(wl_display_get_registry);
   std::cout << "wl_display@1.get_registry: wl_registry=" << wl_registry_id << std::endl;
+
+  LibWindow::Wayland::RingBuffer<uint32_t> rb(10);
+  std::cout << "Size: " << rb.size() << std::endl;
+
+  std::cout << "enqueue 1" << std::endl;
+  rb.enqueue(1);
+  std::cout << "Size: " << rb.size() << std::endl;
+
+  std::cout << "enqueue 2" << std::endl;
+  rb.enqueue(2);
+  std::cout << "Size: " << rb.size() << std::endl;
+
+  std::cout << "enqueue 2" << std::endl;
+  rb.enqueue(2);
+  std::cout << "Size: " << rb.size() << std::endl;
+
+  std::cout << "enqueue 1" << std::endl;
+  rb.enqueue(1);
+  std::cout << "Size: " << rb.size() << std::endl;
+
+  std::cout << "enqueue 2" << std::endl;
+  rb.enqueue(2);
+  std::cout << "Size: " << rb.size() << std::endl;
+
+  std::cout << "enqueue 2" << std::endl;
+  rb.enqueue(2);
+  std::cout << "Size: " << rb.size() << std::endl;
+
+  std::cout << "enqueue 2" << std::endl;
+  rb.enqueue(2);
+  std::cout << "Size: " << rb.size() << std::endl;
+
+  std::cout << "enqueue 2" << std::endl;
+  rb.enqueue(2);
+  std::cout << "Size: " << rb.size() << std::endl;
+  uint32_t b = rb.dequeue();
+  std::cout << "dequeue: " << b << std::endl;
   return 0;
 }
